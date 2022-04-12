873664be2e8832fc9e47adc276b7329fa1b5bb8f
Cassandra Rudig <98493518+TrueQueenBee@users.noreply.github.com>
2022-04-12 17:21:56 +0000

    Update info.rkt

--- domain-coloring/info.rkt
+++ domain-coloring/info.rkt
@@ -1,8 +1,8 @@
 #lang info
 
 (define version "1.0")
-(define deps (list "base"
-                   "gui-lib"
-                   "colors"))
+(define deps '("base"
+               "gui-lib"
+               "colors"))
                    
 (define pkg-authors ('TrueQueenBee))
