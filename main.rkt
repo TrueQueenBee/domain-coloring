64cbb0dec3ae32e62b497cc3ed914b781fa628c0
Cassandra Rudig <98493518+TrueQueenBee@users.noreply.github.com>
2022-04-13 17:51:49 +0000

    Add provisions

--- main.rkt
+++ main.rkt
@@ -3,4 +3,35 @@
 (require "complex-grapher.rkt"
          "complex-grapher-test.rkt")
          
-(provide (all-defined-out))
+(provide sgn/c
+         arity-overlaps?
+         homogenous?
+         sigmoid
+         (rename-out [cartesian-product cartesian-product-2]
+                     [num->byte number->byte]
+                     [dist-est mandelbrot-distance-estimate]
+                     [polyprime polynomial-derivative])
+         mod-extended
+         combine
+         complex-timer%
+         complex-frame%
+         complex-canvas%
+         complex
+         random-complex
+         zeroes
+         zero-pole
+         mandelbrot
+         polynomial
+         newton-step
+         integrate
+         erfz
+         f:
+         >>
+         graph
+         set-res
+         show
+         reset
+         near-multiple?
+         counter
+         redraw
+         help)
